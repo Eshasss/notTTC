902f57682547cec22f1f06363155bd2217d003bd
Eshasss <107683200+Eshasss@users.noreply.github.com>
2023-12-06 22:59:24 +0300

    release 1.0 version

--- server.py
+++ server.py
@@ -24,10 +24,6 @@ class Board():
                     avaliables.append(f"{row},{indx}")
         return avaliables
         
-    def avaliable_move_check(self, move):
-        if self.board[int(move[0])][int(move[2])] == " ":
-            return True
-        return False
 
     def print_board(self):
         pretty_board = "\n______\n"
@@ -59,52 +55,46 @@ class TTT:
         #checks rows
         for i in range(3):
             if len(set(self.board[i])) == 1 and " " not in self.board[i]:
-                self.status = True
+                #self.status = True
                 return True
 
         #check for a win along columns
         for j in range(3):
             if self.board[0][j] == self.board[1][j] == self.board[2][j] and self.board[0][j] != " ":                #self.status = True
+                #self.status = True
                 return True
         # check for a win along diagonals
-        if self.board[0][1] == self.board[1][1] == self.board[2][2] and self.board[0][1] != " ":
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[0][1] != " ":
               
             #self.status = True
             return True
         if self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[0][2] != " ":
-            #self.status = True
+            self.status = True
             return True
         o = 0
         for k in self.board:
             for s in k:
                 if s == " ":
-                    return False
-        return 'Draw'
+                    o += 1
+        if not o:
+            #self.status = 'Draw'
+            return 'Draw'
                 
 
     def manager(self, client_answer=None):
-        #Если ходит сервер то находится лучший ход, игрок - вносится значение хода игрока
-        if self.game_status_checker() == False:
-            if self.turn == self.player1:
-                self.board.make_move(random.choice(self.board.avaliable_move()), "F")
-                text = f"Fishie swam!\n{self.board.print_board()}\n"
-                self.turn = "Player"
-            elif self.turn == self.player2:
-                if client_answer not in self.board.avaliable_move():
-                    text = f"can't make this move!{client_answer} You can go to{self.board.avaliable_move()}"
-                else:
-                    self.board.make_move(client_answer, "P")
-                    self.turn = 'Fishie'
-                    text = f"You made move {client_answer}.\n{self.board.print_board()}\n"
-            return text
-        elif self.game_status_checker() == 'Draw':
-            return f"Damn.. It's a Draw. Bro u need to train more...."
-        else:
-    
-            if self.turn == "Fishie":
-                return f"Game Over! You won Fishie! Congrads! :D"
+        if self.turn == self.player1:
+            self.board.make_move(random.choice(self.board.avaliable_move()), "F")
+            text = f"Fishie move!\n{self.board.print_board()}\n"
+            self.turn = "Player"
+        elif self.turn == self.player2:
+            if client_answer not in self.board.avaliable_move():
+                text = f"can't make this move!{client_answer} You can go to{self.board.avaliable_move()}"
             else:
-                return f"Oppps! Fishie won!"
+                self.board.make_move(client_answer, "P")
+                self.turn = 'Fishie'
+                text = f"You made move {client_answer}.\n{self.board.print_board()}\n"
+        return text
+
     def hoster(self):
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -118,7 +108,17 @@ class TTT:
         client.send(self.manager().encode('UTF-8'))
         
         while True:
-            
+            if self.game_status_checker() == True:
+                if self.turn == "Fishie":
+                    client.send("Game Over! You won Fishie! Congrads! :D".encode('UTF-8'))
+                    break
+                else:
+                    client.send("Game Over! Oppps! Fishie won!".encode('UTF-8'))
+                    break
+            elif self.game_status_checker() == 'Draw':
+                client.send("Game Over! Damn.. It's a Draw. Bro u need to train more....".encode('UTF-8'))
+                break
+
             if self.turn == "Player":
                 client_answer = client.recv(2048).decode('UTF-8')
                 managed = self.manager(client_answer)
--- user.py
+++ user.py
@@ -13,7 +13,7 @@ s.send(client_answer.encode("UTF-8"))
 while True:
     server_send = s.recv(2048).decode('UTF-8')
     print(server_send)
-    if server_send == "Game Over!":
+    if server_send[0] == "G":
         s.close()
         break
     if server_send[0] != "Y":
